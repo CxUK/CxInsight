7edc0310b793e8faeb1c2f6a9152db9532496a52
AndrewAtCheckmarx <andrew.thompson@checkmarx.com>
2022-01-11 13:35:36 +0000

    Add files via upload

---
+++ insight.ps1
@@ -0,0 +1,50 @@
+﻿<#
+.SYNOPSIS
+This is a Powershell script to retrieve your Checkmarx ScanData for Insight Analysis
+.DESCRIPTION
+This script will collect Scan Information that includes data about: Projects, Presets, Teams, Engines, and Result Metrics 
+.PARAMETER cx_sast_server
+    URL of the Checkmarx Server (i.e. https://companyname.checkmarx.net or http://localhost).
+.PARAMETER day_span
+    The amount of days you from current day you would like to retrieve data for.
+.PARAMETER start_date
+    The start date of the date range you would like to collect data (Format: yyyy-mm-DD)
+.PARAMETER end_date
+    The end date of the date range you would like to collect data (Format: yyyy-mm-DD)
+.EXAMPLE
+    C:\PS> .\insight.ps1 -cx_sast_server https://customerurl.checkmarx.net
+.EXAMPLE
+    C:\PS> .\insight.ps1 -start_date 2019-04-01
+.EXAMPLE
+    C:\PS> .\insight.ps1 -cx_sast_server http://localhost -day_span 180
+.NOTES
+    Author: Checkmarx
+    Date:   April 13, 2020
+#>
+
+param(
+    [Parameter(Mandatory=$true)]
+    [String]$cx_sast_server,
+    [int]$day_span = 90,
+    [String]$start_date = ((Get-Date).AddDays(-$day_span).ToString("yyyy-MM-dd")),
+    [String]$end_date = (Get-Date -format "yyyy-MM-dd")
+    ) 
+
+$Cred = Get-Credential -Credential $null
+$Url = "${cx_sast_server}/cxwebinterface/odata/v1/Scans?`$select=Id,ProjectName,OwningTeamId,TeamName,ProductVersion,EngineServerId,Origin,PresetName,ScanRequestedOn,QueuedOn,EngineStartedOn,EngineFinishedOn,ScanCompletedOn,ScanDuration,FileCount,LOC,FailedLOC,TotalVulnerabilities,High,Medium,Low,Info,IsIncremental,IsLocked,IsPublic&`$expand=ScannedLanguages(`$select=LanguageName)&`$filter=ScanRequestedOn%20gt%20${start_date}Z%20and%20ScanRequestedOn%20lt%20${end_date}z"
+
+try
+{
+    $response = Invoke-WebRequest -Uri $Url -Credential $cred -OutFile data.txt -ErrorAction Stop
+    # This will only execute if the Invoke-WebRequest is successful.
+    $StatusCode = $Response.StatusCode
+    Read-Host -Prompt "The script was successful. Please send the 'data.txt' file in this directory to your Checkmarx Engineer. Press Enter to exit"
+}
+catch
+{
+    Write-Error $_.Exception.ToString()
+    Write-Host "StatusCode:" $_.Exception.Response.StatusCode.value__ 
+    Write-Host "StatusDescription:" $_.Exception.Response.StatusDescription
+    Read-Host -Prompt "The above error occurred. Press Enter to exit."
+}
+
